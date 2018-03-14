e83302be392d08e2a9493651efd455f3f8fb0dc4
jen_zim <jzimmerman@shoppertrak.com>
2018-03-14 15:11:58 -0500

    Added/wired recipe-list and recipe-detail components, recipe service, bootstrap, angular logic for interacting with recipe details.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 
 import { AppComponent } from './app.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RecipeService } from './recipe.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    RecipeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/recipe.ts
@@ -0,0 +1,11 @@
+export class Recipe {
+  id: number;
+  name: string;
+  category: string;
+  ingredients: string[];
+  numberOfServings: string;
+  instructions: string[];
+  dateCreated: Date;
+  dateModified: Date;
+  notes: string;
+}
